4c4113a5bee76852ad0fbb882d235cb57e941d0f
Sharvesh K <sharveshk1228@gmail.com>
2025-11-16 05:21:07 +0530

    po

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import google.generativeai as genai
 # =======================================
 api_key = "AIzaSyCyUDvhpsAU3LlwcfPcvSC-3YUniD31Fl8"
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-flash-002")
 
 # =======================================
 # Abusive Words List
@@ -149,6 +149,7 @@ with tab2:
 
         st.markdown("### 📅 Latest User Messages")
         st.dataframe(df[["user", "last_message", "last_time"]])
+
 
 
 
